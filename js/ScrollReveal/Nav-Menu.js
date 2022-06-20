bb9dbc0cc4cc7aae29d03967d1ab44ca20b0c7ab
REMR17 <remr11@outlook.com>
2022-06-20 07:19:16 -0500

    V2 Actualizando animaciones

---
+++ js/ScrollReveal/Nav-Menu.js
@@ -0,0 +1,25 @@
+window.sr = ScrollReveal().reveal(".menu-body", {
+  origin: "left",
+  distance: "200px",
+  duration: 2000,
+  opacity: 0,
+});
+
+window.sr = ScrollReveal().reveal(".menu-options", {
+  delay: 1100,
+  origin: "bottom",
+  distance: "250px",
+  duration: 1000,
+});
+
+window.sr = ScrollReveal().reveal(".menu-labels", {
+  delay: 2000,
+  scale: 0,
+});
+
+window.sr = ScrollReveal().reveal(".menu-rocket", {
+  delay: 1100,
+  origin: "bottom",
+  distance: "500px",
+  dutation: 1000,
+});
---
+++ js/ScrollReveal/div-body.js
@@ -0,0 +1,30 @@
+window.sr = ScrollReveal().reveal(".body-title-home", {
+  origin: "bottom",
+  distance: "200px",
+  duration: 2000,
+  opacity: 0,
+});
+
+window.sr = ScrollReveal().reveal(".body-card-bottom", {
+  delay: 1000,
+  origin: "bottom",
+  distance: "200px",
+  duration: 2000,
+  opacity: 0,
+});
+
+window.sr = ScrollReveal().reveal(".body-card-left", {
+  delay: 1000,
+  origin: "left",
+  distance: "200px",
+  duration: 2000,
+  opacity: 0,
+});
+
+window.sr = ScrollReveal().reveal(".body-card-right", {
+  delay: 1000,
+  origin: "right",
+  distance: "200px",
+  duration: 2000,
+  opacity: 0,
+});
