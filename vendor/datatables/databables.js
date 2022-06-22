a1531d5fe482aefb972ee7365dd177da235bc974
REMR17 <remr11@outlook.com>
2022-06-21 19:48:24 -0500

    V2 Funcionalidad tablas sin finalizar

--- vendor/datatables/databables.js
+++ vendor/datatables/databables.js
@@ -1,31 +1,27 @@
 var n = 0;
 
-function generate() {
-  n++;
-  //Obtener datos
-  var dato = document.getElementById("dato").value;
-  var caja = document.getElementById("caja");
-  
-
-  
-  
-  //Crean objetos
-  var contenedor = document.createElement('tr');
-  var numero = document.createElement("th");
-  var btn1 = document.createElement('input');
-  var btn2 = document.createElement('input');
-  
-  //Propiedades
-  texto.innerHTML = n + " - "+ dato
-  btn1.name="btn"+n;
-  btn2.name="btn"+n;
-  btn1.type="radio";
-  btn2.type="radio";
-  
-  //Herencia
-  contenedor.appendChild(texto);
-  contenedor.appendChild(btn1);
-  contenedor.appendChild(btn2);
-  caja.appendChild(contenedor);
-  
+function createTest() {
+  var number =  document.getElementsByName("inputNie").values;
+  var caja = document.getElementsByName("caja").getelementsByTagsName("tr").lenght-1;
+  console.log(caja);
 }
+window.addEventListener("", {
+ 
+ 
+});
+
+$(document).ready(function(createTest) {
+  var nrows = 0; var nres = 0;
+     $("table tr").each(function() {
+         nrows++;
+     })
+     $("#nrows").html(nrows);
+     $(".isres").each(function() {
+         nres++;
+     })
+     $("#nres").html(nres);
+ });
+ console.log(nrows);
+
+ //esto estaba viendo
+ //https://es.stackoverflow.com/questions/335592/contar-filas-de-una-tabla-html
