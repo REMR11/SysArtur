387e03f2c612b9f4f8e5bb404e9e599ea4efd137
REMR17 <remr11@outlook.com>
2022-06-22 20:08:24 -0500

    V2.4 Funcionalidad tabla

--- vendor/datatables/databables.js
+++ vendor/datatables/databables.js
@@ -1,24 +1,43 @@
 var n = 0;
+var result = 0;
 
 function createTest() {
-  const number = document.getElementById("inputNie").values;
+  const number = document.getElementById("inputNie").value;
   const box = document.getElementById("caja");
-  var result = 0;
-  console.log(number);
+  var titulo = document.getElementById("title-table");
+  var fila = document.getElementById("fila");
+
+  console.log("Numero de columnas: " + number);
 
   if (
     typeof box === "object" &&
     box !== null &&
     "getElementsByTagName" in box
-    ) {
-      result = box.getElementsByTagName("tr");
+  ) {
+    result = box.getElementsByTagName("tr");
+  }
+
+  var tCelda = document.createElement("th");
+  var celda = document.createElement("td");
+  var dato = document.createElement("input");
+
+  dato.type = "number";
+  dato.placeholder = 0;
+  dato.value = 0;
+  tCelda.innerHTML = "Eva-" + n;
+
+  if (result[n] === 0) {
+    titulo.appendChild(tCelda);
+  } else if(result[n]<= result.length){
     
+    for (let i = n; i <= result.length; i++) {
+      celda.appendChild(dato);
+      fila.appendChild(celda);
     }
     
-    console.log(result.length);
+  }
 
-
-
+  console.log("Numeros de filas: " + result.length);
 }
 
 //esto estaba viendo
